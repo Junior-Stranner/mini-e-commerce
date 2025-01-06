e06b58f4431d65e82fe352ea7fd3b137b2c3a789
Junior-Stranner <JuniorStranner@gmail.com>
2025-01-06 11:42:38 -0300

    already created in Product Controller create and update

---
+++ back-end/src/main/java/br/com/judev/backend/controller/ProductController.java
@@ -0,0 +1,38 @@
+package br.com.judev.backend.controller;
+
+import br.com.judev.backend.dto.ProductDTO;
+import br.com.judev.backend.services.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ProductDTO> createProduct(@RequestPart("product") @Valid ProductDTO productDTO,
+                                                    @RequestPart(value = "image", required = false) MultipartFile image) throws IOException {
+        return ResponseEntity.ok(productService.createProduct(productDTO, image));
+    }
+
+
+    @PutMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long id,
+                                                    @RequestPart("product") @Valid ProductDTO productDTO,
+                                                    @RequestPart(value = "image", required = false) MultipartFile image)throws IOException{
+        return ResponseEntity.ok(productService.updateProduct(id, productDTO, image));
+    }
+
+}
