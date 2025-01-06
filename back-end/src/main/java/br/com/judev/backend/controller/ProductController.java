53f97e21b5636df5202f8217c19beffd934a1a0c
Junior-Stranner <JuniorStranner@gmail.com>
2025-01-06 14:24:14 -0300

    finished to add all the Product Conrtoller

--- back-end/src/main/java/br/com/judev/backend/controller/ProductController.java
+++ back-end/src/main/java/br/com/judev/backend/controller/ProductController.java
@@ -1,9 +1,13 @@
 package br.com.judev.backend.controller;
 
 import br.com.judev.backend.dto.ProductDTO;
+import br.com.judev.backend.dto.ProductListDTO;
 import br.com.judev.backend.services.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -33,6 +37,23 @@ public class ProductController {
                                                     @RequestPart("product") @Valid ProductDTO productDTO,
                                                     @RequestPart(value = "image", required = false) MultipartFile image)throws IOException{
         return ResponseEntity.ok(productService.updateProduct(id, productDTO, image));
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProduct(@PathVariable Long id){
+        return ResponseEntity.ok(productService.getProduct(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductListDTO>> getAllProducts(@PageableDefault(size=10) Pageable pageable){
+        return ResponseEntity.ok(productService.getAllProducts(pageable));
     }
 
 }
