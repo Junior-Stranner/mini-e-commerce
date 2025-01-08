046d8c90e6fd20277894ca6ae6f4909e654556d8
Junior-Stranner <JuniorStranner@gmail.com>
2025-01-08 17:12:55 -0300

    end with Cart

--- back-end/src/main/java/br/com/judev/backend/controller/CartController.java
+++ back-end/src/main/java/br/com/judev/backend/controller/CartController.java
@@ -1,10 +1,12 @@
 package br.com.judev.backend.controller;
 
 import br.com.judev.backend.dto.CartDTO;
+import br.com.judev.backend.model.User;
 import br.com.judev.backend.services.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,5 +31,23 @@ public class CartController {
     public ResponseEntity<CartDTO> getCart(@AuthenticationPrincipal UserDetails userDetails){
         Long userId = ((User) userDetails).getId();
         return ResponseEntity.ok(cartService.getCart(userId));
+    }
+
+    @DeleteMapping
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Void> clearCart(@AuthenticationPrincipal UserDetails userDetails){
+        Long userId = ((User) userDetails).getId();
+        cartService.clearCart(userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    //update
+    @DeleteMapping("/{productId}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Void> removeCartItem(@AuthenticationPrincipal UserDetails userDetails,
+                                               @PathVariable Long productId) {
+        Long userId = ((User) userDetails).getId();
+        cartService.removeCartItem(userId, productId);
+        return ResponseEntity.noContent().build();
     }
 }
