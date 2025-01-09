cfb5ebaa0352855957b3c82962fe73a72e81f5df
Junior-Stranner <JuniorStranner@gmail.com>
2025-01-09 09:04:13 -0300

    Email Service Complete

---
+++ back-end/src/main/java/br/com/judev/backend/services/OrderService.java
@@ -0,0 +1,9 @@
+package br.com.judev.backend.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+}
